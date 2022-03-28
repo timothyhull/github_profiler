809ddb8099c9ce9985dc1d815ebd731c176083bc
Timothy Hull <timothyhull@gmail.com>
2022-03-28 22:24:42 +0000

    Add Flask.run function, .env vars, and constants

--- app/app.py
+++ app/app.py
@@ -2,11 +2,22 @@
 """ GitHub profiler web application for #100DaysofCode Days 59+60. """
 
 # Imports - Python Standard Library
+from os import getenv
 
 # Imports - Third-Party
+from dotenv import load_dotenv
 from flask import Flask
 
 # Imports - Local
+
+# Load environment variables
+load_dotenv()
+
+
+# Constants
+FLASK_DEBUG = getenv('FLASK_DEBUG') or False
+FLASK_HOST = getenv('FLASK_HOST') or 'localhost'
+FLASK_PORT = getenv('FLASK_PORT') or 5000
 
 # Flask web application object
 app = Flask(__name__)
@@ -28,3 +39,10 @@ def default_route() -> str:
     output = 'This is a test.'
 
     return output
+
+
+Flask.run(
+    host=FLASK_HOST,
+    port=FLASK_PORT,
+    debug=FLASK_DEBUG
+)
