4899910f21d158bc76292d3dac65a99f654c5f7b
Timothy Hull <timothyhull@gmail.com>
2022-04-02 00:41:45 +0000

    Add initial test code

--- app/github_profiler.py
+++ app/github_profiler.py
@@ -2,7 +2,24 @@
 """ GitHub profiler application for #100DaysofCode Days 59+60. """
 
 # Imports - Python Standard Library
+from os import getenv
 
 # Imports - Third-Party
+from dotenv import load_dotenv
+from github import Github
 
 # Imports - Local
+
+# Load environment variables
+load_dotenv()
+
+# Constants
+GITHUB_TOKEN = getenv('GITHUB_TOKEN')
+
+# Test Github object
+gh = Github(
+    login_or_token=GITHUB_TOKEN
+)
+
+# Display GitHub rate limits
+print(gh.rate_limiting)
