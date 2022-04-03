61e9f4efffc11465aaaa40ee5694c910cbe6f707
Timothy Hull <timothyhull@gmail.com>
2022-04-03 07:06:55 +0000

    Start refactoring for consumption via functions

--- app/github_profiler.py
+++ app/github_profiler.py
@@ -16,10 +16,28 @@ load_dotenv()
 # Constants
 GITHUB_TOKEN = getenv('GITHUB_TOKEN')
 
-# Test Github object
-gh = Github(
-    login_or_token=GITHUB_TOKEN
-)
 
-# Display GitHub rate limits
-print(gh.rate_limiting)
+# Functions
+def github_auth(
+    github_token: str = None
+) -> Github:
+    """ Create a Github object with public or login/token auth.
+
+        Args:
+            github_token (str, optional):
+                Github user token for user authentication.  Default
+                value is None, which provides public authentication,
+                with 60 API requests per hour.  Token-authenticated
+                Github objects have 5,000 API requests per hour.
+
+        Returns:
+            github_object (github.Github):
+                Instance of the github.Github class.
+    """
+
+    # Create a Github object
+    github_object = Github(
+        login_or_token=github_token
+    )
+
+    return github_object
