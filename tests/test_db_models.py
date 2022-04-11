b308b0fd60d6037fa5fde6e9e7af160770de3279
Timothy Hull <timothyhull@gmail.com>
2022-04-11 23:39:19 +0000

    Update test with attribute and corresponding repr values

--- tests/test_db_models.py
+++ tests/test_db_models.py
@@ -9,3 +9,32 @@
 from db.db_models import Repos
 
 # Constants
+REPO_NAME = 'test_repo'
+REPO_OWNER = 'timothyhull'
+REPO_URL = f'https://github.com/{REPO_OWNER}/{REPO_NAME}'
+REPOS_REPR_STRING = (
+    f'<Repo(name={REPO_NAME}, owner={REPO_OWNER}, url={REPO_URL})>'
+)
+
+
+# Test functions
+def test_repos() -> None:
+    """ Test the Repos class.
+
+        Determine if the Repos.__repr__ method returns the expected
+        string value.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+    """
+
+    # Create an instance of the Repos class and set attributes
+    repo = Repos()
+    repo.name = REPO_NAME
+    repo.owner = REPO_OWNER
+    repo.url = REPO_URL
+
+    assert str(repo) == REPOS_REPR_STRING
