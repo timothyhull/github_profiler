fc9a1a8606862086518885e9e0b56adc40b4cce3
Timothy Hull <timothyhull@gmail.com>
2022-04-19 05:51:09 +0000

    Rename files

--- db/db_helper.py
+++ db/db_helper.py
@@ -9,8 +9,11 @@ from sqlite3 import connect
 
 # Imports - Third-Party
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 # Imports - Local
+# from db.db_models import BASE, Repos
 
 # Load environment variables
 load_dotenv()
@@ -18,13 +21,27 @@ load_dotenv()
 # Constants
 CURRENT_DIR = Path(dirname(__file__))
 CURRENT_DIR_ABSPATH = abspath(CURRENT_DIR)
-DB_URL = getenv(key='DB_URL', default=None)
+DB_ECHO = True
+DB_FUTURE = True
 DB_NAME = getenv(key='DB_NAME', default=None)
 DB_TEST_NAME = getenv(key='TEST_DB_NAME', default=None)
+DB_URL = getenv(key='DB_URL', default=None)
 
 # Create a sqllite3 database connection, and database if none exists
 conn = connect(
     database=join(CURRENT_DIR_ABSPATH, DB_NAME)
+)
+
+# Create an SQLAlchemy database engine object
+engine = create_engine(
+    url=DB_URL,
+    echo=DB_ECHO,
+    future=True
+)
+
+# Create an SQLAlchemy database session
+session = Session(
+    engine=engine
 )
 
 # Functions
