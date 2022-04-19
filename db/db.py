89b7a28247bbf7fc35350d6343b466dfc6021829
Timothy Hull <timothyhull@gmail.com>
2022-04-19 05:40:45 +0000

    Relocate sqllite3 import line

--- db/db.py
+++ db/db.py
@@ -5,12 +5,12 @@
 from os import getenv
 from os.path import abspath, dirname, join
 from pathlib import Path
+from sqlite3 import connect
 
 # Imports - Third-Party
 from dotenv import load_dotenv
 
 # Imports - Local
-from sqlite3 import connect
 
 # Load environment variables
 load_dotenv()
@@ -22,7 +22,7 @@ DB_URL = getenv(key='DB_URL', default=None)
 DB_NAME = getenv(key='DB_NAME', default=None)
 DB_TEST_NAME = getenv(key='TEST_DB_NAME', default=None)
 
-# Create a database connection
+# Create a sqllite3 database connection, and database if none exists
 conn = connect(
     database=join(CURRENT_DIR_ABSPATH, DB_NAME)
 )
