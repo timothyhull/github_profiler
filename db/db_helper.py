acc6cfe48f54e4e23e443a46d941dd3ca375af8a
Timothy Hull <timothyhull@gmail.com>
2022-04-20 01:52:08 +0000

    Refactor existing code as a function. Returns a session object.

--- db/db_helper.py
+++ db/db_helper.py
@@ -6,14 +6,16 @@ from os import getenv
 from os.path import abspath, dirname, join
 from pathlib import Path
 from sqlite3 import connect
+from sys import argv
 
 # Imports - Third-Party
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session as SessionObject
+from sqlalchemy.orm import sessionmaker
 
 # Imports - Local
-from db.db_models import BASE, Repos
+from db.db_models import BASE
 
 # Load environment variables
 load_dotenv()
@@ -21,33 +23,70 @@ load_dotenv()
 # Constants
 CURRENT_DIR = Path(dirname(__file__))
 CURRENT_DIR_ABSPATH = abspath(CURRENT_DIR)
-DB_ECHO = True
-DB_FUTURE = True
+DB_AUTOFLUSH = True
+DB_LOGGING = True
+DB_FUTURE_COMPATIBLE = True
 DB_NAME = getenv(key='DB_NAME', default=None)
 DB_TEST_NAME = getenv(key='TEST_DB_NAME', default=None)
+DB_TEST_URL = getenv(key='DB_TEST_URL', default=None)
 DB_URL = getenv(key='DB_URL', default=None)
 
 # Create a sqllite3 database connection, and database if none exists
-conn = connect(
+connect(
     database=join(CURRENT_DIR_ABSPATH, DB_NAME)
 )
 
-# Create an SQLAlchemy database engine object
-engine = create_engine(
-    url=DB_URL,
-    echo=DB_ECHO,
-    future=True
+
+# Classes
+Session = sessionmaker(
+    autoflush=DB_AUTOFLUSH
 )
 
-# Create an SQLAlchemy database session
-session = Session(engine)
-
-# Create database tables, if they don't already exist
-BASE.metadata.create_all(engine)
-
-# Query the database Repos table
-session.query(
-    Repos
-).all()
 
 # Functions
+def _create_session() -> SessionObject:
+    """ Create and Bind an Engine object to a Session object.
+        Add an sqlalchemy.engine.Engine binding to the custom
+        Session class.
+
+        Args:
+            None.
+
+        Returns:
+            session (sqlalchemy.orm.Session as SessionObject):
+                Instance of the Session class with an engine binding.
+    """
+
+    # Set the context appropriate DB URL
+    if 'pytest' in argv[0]:
+        db_url = DB_TEST_URL
+    else:
+        db_url = DB_URL
+
+    # Verify db_url is not None
+    if db_url is None:
+        raise EnvironmentError(
+            '\nSet the "DB_URL" and/or "DB_TEST_URL" environment variables\n'
+        )
+
+    # Create an sqlalchemy.engine.Engine object
+    engine = create_engine(
+        url=db_url,
+        echo=DB_LOGGING,
+        future=DB_FUTURE_COMPATIBLE
+    )
+
+    # Create database tables, if they don't already exist
+    BASE.metadata.create_all(
+        bind=engine
+    )
+
+    # Bind the engine object to a Session object
+    Session.configure(
+        bind=engine
+    )
+
+    # Create a session class instance
+    session = Session()
+
+    return session
