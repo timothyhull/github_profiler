397cb260f9fa5232ffb008539c8d39af5c46d6f8
Timothy Hull <timothyhull@gmail.com>
2022-04-21 05:52:16 +0000

    Update Repos docstring, add 'private' attribute

--- db/db_models.py
+++ db/db_models.py
@@ -5,7 +5,7 @@
 
 # Imports - Third-Party
 from sqlalchemy import (
-    Column, DateTime, Integer, String
+    Boolean, Column, DateTime, Integer, String
 )
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -21,6 +21,25 @@ class Repos(BASE):
 
         Args:
             BASE (sqlalchemy.ext.declarative.declarative_base).
+
+        Attributes:
+            name (str):
+                Repository name.
+
+            description (str),
+                Repository description.
+
+            private (bool):
+                Private repository status.
+
+            owner (str):
+                Repository owner.
+
+            url (str):
+                Repository URL
+
+            updated_at (datetime.datetime.utcnow):
+                Timestamp for the last repository update.
 
         Returns:
             repr_string (str):
@@ -38,23 +57,27 @@ class Repos(BASE):
     name = Column(String(50))
     description = Column(String(100))
     owner = Column(String(40))
-    url = Column(String(length=50))
+    private = Column(Boolean)
+    url = Column(String(50))
     updated_at = Column(DateTime)
 
     # Define repr function
     def __repr__(self):
         """ Function that returns the repo name and owner.
+
             Args:
                 None.
+
             Returns:
                 repr_string (str):
-                    Returns a string with the hashtag and hashtag
-                    count.
+                    Returns a string with the repo name,
+                    owner, private status, and URL.
         """
 
         # Create a repr string
         repr_string = (
-            f'<Repo(name={self.name}, owner={self.owner}, url={self.url})>'
+            f'<Repo(name={self.name}, owner={self.owner}, '
+            f'private={self.private}, url={self.url})>'
         )
 
         return repr_string
